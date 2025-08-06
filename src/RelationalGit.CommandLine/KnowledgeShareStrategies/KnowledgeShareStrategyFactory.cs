0bce8c7dfc77a32900cbc63c7b8dee8f7a8e8a6a
Mohammadali Sefidi Esfahani <mohammadali.sefidi@gmail.com>
2025-08-06 17:38:17 +0330

    Update KnowledgeShareStrategyFactory.cs
    Add the commands for RAR, RARSofiaWL, and AcHRev recommenders

--- src/RelationalGit.CommandLine/KnowledgeShareStrategies/KnowledgeShareStrategyFactory.cs
+++ src/RelationalGit.CommandLine/KnowledgeShareStrategies/KnowledgeShareStrategyFactory.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using RelationalGit.Recommendation;
 using RelationalGit.Simulation;
 using System;
@@ -29,6 +29,10 @@ namespace RelationalGit.KnowledgeShareStrategies
             {
                 return new CHRevRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
             }
+            else if (recommendationStrategy == KnowledgeShareStrategyType.AuthorshipCHRev)
+            {
+                return new AuthorshipCHRevRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
+            }
             else if (recommendationStrategy == KnowledgeShareStrategyType.RevOwnRec)
             {
                 return new RevOwnRecRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
@@ -58,23 +62,36 @@ namespace RelationalGit.KnowledgeShareStrategies
                 return new ContributionRecRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
             }
 
+            else if (recommendationStrategy == KnowledgeShareStrategyType.RAR)
+            {
+                return new RARRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, numberOfPeriodsForCalculatingProbabilityOfStay, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
+            }
+            
+            else if (recommendationStrategy == KnowledgeShareStrategyType.RARSofiaWL)
+            {
+                return new RARSofiaWLRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, numberOfPeriodsForCalculatingProbabilityOfStay, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
+            }
+
             else if (recommendationStrategy == KnowledgeShareStrategyType.WhoDoRec)
             {
                 return new WhoDoRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, numberOfPeriodsForCalculatingProbabilityOfStay, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
             }
+
             else if (recommendationStrategy == KnowledgeShareStrategyType.SofiaWLRec)
             {
                 return new SofiaWLRecommendationStrategy(knowledgeSaveReviewerReplacementType, logger, numberOfPeriodsForCalculatingProbabilityOfStay, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
             }
+
             else if (recommendationStrategy == KnowledgeShareStrategyType.WhoDoWithCommit)
             {
                 return new WhoDoWithCommitWorkShareStrategy(knowledgeSaveReviewerReplacementType, logger, numberOfPeriodsForCalculatingProbabilityOfStay, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
             }
+
             else if (recommendationStrategy == KnowledgeShareStrategyType.OwnerShipKnowledgeShare)
             {
                 return new OwnerShipKnowledgeShareStrategy(knowledgeSaveReviewerReplacementType, logger, pullRequestReviewerSelectionStrategy, addOnlyToUnsafePullrequests, recommenderOption, changePast, simulationType);
-            } 
+            }
                 throw new ArgumentException($"invalid {nameof(recommendationStrategy)}");
+            }
         }
     }
-}
